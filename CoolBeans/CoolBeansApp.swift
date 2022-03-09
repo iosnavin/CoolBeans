2fac6695f2e32e449b05cbbb46be9a524dc5caa6
Naveen <ios.nreddy@gmail.com>
2022-03-09 11:41:33 +0530

    Initial Commit

---
+++ CoolBeans/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  CoolBeans
+//
+//  Created by Naveen Reddy on 09/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
